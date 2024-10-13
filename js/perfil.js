b78399d3f74ff1e25ef6fe55cfad92b0f8b59156
TOSHIBA <Maria14@>
2024-10-13 17:08:34 -0500

    perfil

---
+++ js/perfil.js
@@ -0,0 +1,11 @@
+// Función para editar el perfil
+function editProfile() {
+    alert('Funcionalidad para editar el perfil aún no implementada.');
+}
+
+// Función para cerrar sesión
+function logout() {
+    alert('Cerrando sesión...');
+    
+    window.location.href = 'muro.html'; 
+}
